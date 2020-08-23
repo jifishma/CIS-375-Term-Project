c3adc6f5ce7f654a1dddb53fbe8aa266a35a1a16
jifishma <44071786+jifishma@users.noreply.github.com>
2020-08-22 23:36:26 -0400

    Some final documentation updates
    End of semester submission

--- Sourcecode/Hotel Management System/Entities/DatabaseEntities.Designer.cs
+++ Sourcecode/Hotel Management System/Entities/DatabaseEntities.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\jifis\OneDrive\School Documents\2020 Summer\CIS 375\Sourcecode\Hotel Management System\Entities\DatabaseEntities.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\jifis\Documents\GitHub\CIS-375-Term-Project\Sourcecode\Hotel Management System\Entities\DatabaseEntities.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
